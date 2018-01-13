2d5b4fd191e825ab0eaa18f39ac95391dc91e280
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-13 00:05:19 -0600

    Set up for testing with jasmine

---
+++ Javascript_Solutions/CodeWars_NextBiggerNumber/nextBigger.js
@@ -0,0 +1,12 @@
+export const splitToArray = num => num.toString().split("");
+export const sortArray = array => array.sort();
+export const findNextLargerCombination = (numArray, sortedArray) => {
+  let nextLarger = [];
+  for (let i = 0, len = array.length; i < len; i++) {
+    if (sortedArray[i] >= numArray[0]) {
+    }
+  }
+};
+
+function nextBigger(n) {}
+export default nextBigger;
---
+++ Javascript_Solutions/CodeWars_NextBiggerNumber/spec.js
@@ -0,0 +1,24 @@
+import nextBigger from "./nextBigger";
+import * as methods from "./nextBigger";
+
+xdescribe("nextBigger", () => {
+  it("should accept a positive integer number and return the next bigger number formed by the same digits", () => {
+    expect(nextBigger(12)).toBe(21);
+    expect(nextBigger(513)).toBe(531);
+    expect(nextBigger(2017)).toBe(2071);
+    expect(nextBigger(414)).toBe(441);
+    expect(nextBigger(144)).toBe(414);
+  });
+});
+
+describe("splitToArray", () => {
+  it("should return an array", () => {
+    expect(methods.splitToArray(123)).toEqual(["1", "2", "3"]);
+  });
+});
+
+describe("sortArray", () => {
+  it("should return a sorted array", () => {
+    expect(methods.sortArray(["2", "5", "3", "4"])).toEqual(["2", "3", "4", "5"]);
+  });
+});
