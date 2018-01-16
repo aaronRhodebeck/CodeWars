6c55163ffd77f27214e8f725bed57c2938b43282
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-16 00:10:59 -0600

    Passes all sample tests

--- Javascript_Solutions/CodeWars_NestingStructureComparison/Kata.js
+++ Javascript_Solutions/CodeWars_NestingStructureComparison/Kata.js
@@ -1,6 +1,19 @@
 Array.prototype.sameStructureAs = function(other) {
-  // Return 'true' if and only if 'other' has the same
-  // nesting structure as 'this'.
-  // Note: You are given a function isArray(o) that returns
-  // whether its argument is an array.
+  return compareArrays(this, other);
 };
+
+function compareArrays(arr1, arr2) {
+  for (let i = 0, len = arr1.length; i < len; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      return compareArrays(arr1[i], arr2[i]);
+    } else if (
+      (Array.isArray(arr1[i]) && !Array.isArray(arr2[i])) ||
+      (!Array.isArray(arr1[i]) && Array.isArray(arr2[i]))
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export default Array;
--- Javascript_Solutions/CodeWars_NestingStructureComparison/spec.js
+++ Javascript_Solutions/CodeWars_NestingStructureComparison/spec.js
@@ -1,12 +1,29 @@
 // Test driven development is recommended for this kata
+import Array from "./Kata";
 
-describe("Sample Tests", () => {
+fdescribe("sameStructureAs", () => {
+  it("should return a boolean", () => {
+    expect([].sameStructureAs([])).toEqual(jasmine.any(Boolean));
+  });
+  describe("compareArrays", () => {
+    it("should return false if the arrays dont match", () => {
+      expect([1].sameStructureAs([[1]])).toBe(false);
+      expect([[1]].sameStructureAs([1])).toBe(false);
+    });
+    it("should ignore non-arrays", () => {
+      expect([1].sameStructureAs([2])).toBe(true);
+      expect([[2]].sameStructureAs([[1]])).toBe(true);
+    });
+  });
+});
+
+fdescribe("Sample Tests", () => {
   it("should pass all these tests before submitting", () => {
     expect([1, 1, 1].sameStructureAs([2, 2, 2])).toBe(true);
     expect([1, [1, 1]].sameStructureAs([2, [2, 2]])).toBe(true);
     expect([1, [1, 1]].sameStructureAs([[2, 2], 2])).toBe(false);
     expect([1, [1, 1]].sameStructureAs([[2], 2])).toBe(false);
     expect([[[], []]].sameStructureAs([[[], []]])).toBe(true);
-    expect([[[], []]].sameStructureAs([[1, 1]])).toBe(true);
+    expect([[[], []]].sameStructureAs([[1, 1]])).toBe(false);
   });
 });
