c9562c69f00bc406ef5b8f7a71b78e63882f4c70
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-18 01:11:24 -0600

    Snail sort submitted

--- Javascript_Solutions/CodeWars_NestingStructureComparison/spec.js
+++ Javascript_Solutions/CodeWars_NestingStructureComparison/spec.js
@@ -1,7 +1,7 @@
 // Test driven development is recommended for this kata
 import Array from "./Kata";
 
-fdescribe("sameStructureAs", () => {
+describe("sameStructureAs", () => {
   it("should return a boolean", () => {
     expect([].sameStructureAs([])).toEqual(jasmine.any(Boolean));
   });
@@ -18,7 +18,7 @@ fdescribe("sameStructureAs", () => {
   });
 });
 
-fdescribe("Sample Tests", () => {
+describe("Sample Tests", () => {
   it("should pass all these tests before submitting", () => {
     expect([1, 1, 1].sameStructureAs([2, 2, 2])).toBe(true);
     expect([1, [1, 1]].sameStructureAs([2, [2, 2]])).toBe(true);
---
+++ Javascript_Solutions/CodeWars_SnailSort/spec.js
@@ -0,0 +1,42 @@
+// Test driven development is recommended for this kata
+
+import snail from "./Kata";
+
+fdescribe("Sample tests", () => {
+  it("should return the outermost elements to the innermost, traveling clockwise", () => {
+    expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+    expect(snail([[1, 2, 3, 1], [4, 5, 6, 4], [7, 8, 9, 7], [7, 8, 9, 7]])).toEqual([
+      1,
+      2,
+      3,
+      1,
+      4,
+      7,
+      7,
+      9,
+      8,
+      7,
+      7,
+      4,
+      5,
+      6,
+      9,
+      8
+    ]);
+  });
+});
+
+describe("snail()", () => {
+  it("should return an array", () => {
+    expect(snail([])).toEqual(jasmine.any(Array));
+  });
+  it("should return an array with the first array in order", () => {
+    expect(snail([[1, 2, 3]])).toEqual([1, 2, 3]);
+  });
+  it("should make the next number the last number in the second array", () => {
+    expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])[3]).toEqual(6);
+  });
+  it("should reverse the last array", () => {
+    expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).slice(4, 7)).toEqual([9, 8, 7]);
+  });
+});
