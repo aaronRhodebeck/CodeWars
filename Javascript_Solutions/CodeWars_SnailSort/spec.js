c3b8b60ec9a19a6eafaee9d59d2d868101ef77f3
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-18 00:03:38 -0600

    Initial setup

--- Javascript_Solutions/CodeWars_SnailSort/spec.js
+++ Javascript_Solutions/CodeWars_SnailSort/spec.js
@@ -2,8 +2,14 @@
 
 import snail from "./Kata";
 
-fdescribe("Sample tests", () => {
+describe("Sample tests", () => {
   it("should return the outermost elements to the innermost, traveling clockwise", () => {
     expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+});
+
+fdescribe("snail()", () => {
+  it("should return an array", () => {
+    expect(snail([])).toEqual(jasmine.any("Array"));
   });
 });
