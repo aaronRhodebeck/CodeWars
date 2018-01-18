d3df4aab58f7c0ee370e590b6f9a40738055b09a
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-18 01:04:50 -0600

    Sample tests pass

--- Javascript_Solutions/CodeWars_SnailSort/Kata.js
+++ Javascript_Solutions/CodeWars_SnailSort/Kata.js
@@ -1,3 +1,33 @@
 export default function snail(array) {
-  // enjoy
+  let finalAnswer = [];
+  for (let i = 0, len = array.length / 2; i < len; i++) {
+    let currentArray = trimForRound(array, i);
+    finalAnswer = finalAnswer.concat(oneRound(currentArray));
+  }
+  return finalAnswer;
+}
+
+function trimForRound(array, round) {
+  let trimmedArray = [];
+  trimmedArray = array.slice(round, array.length - round);
+  for (let i = 0, len = trimmedArray.length; i < len; i++) {
+    trimmedArray[i] = trimmedArray[i].slice(round, trimmedArray[i].length - round);
+  }
+  return trimmedArray;
+}
+
+function oneRound(arrayOfArrays) {
+  let firstHalf = [];
+  let secondHalf = [];
+  for (let i = 0, len = arrayOfArrays.length; i < len; i++) {
+    if (i === 0) {
+      firstHalf = firstHalf.concat(arrayOfArrays[i]);
+    } else if (i !== len - 1) {
+      firstHalf = firstHalf.concat(arrayOfArrays[i].slice(-1));
+      secondHalf.unshift(arrayOfArrays[i][0]);
+    } else if (i === len - 1) {
+      firstHalf = firstHalf.concat(arrayOfArrays[i].reverse());
+    }
+  }
+  return firstHalf.concat(secondHalf);
 }
