1b0bd7301741609a7243efa3cfc3ec0b4b603e2b
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-17 23:16:31 -0600

    Initial setup

---
+++ Javascript_Solutions/CodeWars_SnailSort/spec.js
@@ -0,0 +1,9 @@
+// Test driven development is recommended for this kata
+
+import snail from "./Kata";
+
+fdescribe("Sample tests", () => {
+  it("should return the outermost elements to the innermost, traveling clockwise", () => {
+    expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+});
